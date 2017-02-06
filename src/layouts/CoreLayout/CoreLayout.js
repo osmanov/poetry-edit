073c8e0fbeb4626bbb48e961d9398dc0756200a0
osmanov <rhenium.osmium@gmail.com>
2017-02-06 15:45:57 +0300

    provider data

---
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import Header from '../../components/Header'
+import styled,{ ThemeProvider } from 'styled-components';
+import EditorMenu from 'components/EditorMenu'
+import './CoreLayout.css'
+
+
+
+const provider = {
+    className:'menuContainer',//<--set classname if you want
+    volume:'up',
+    axis:'x',
+    items:[
+
+      {
+        label:<span>PRESSI</span>,
+        className:'',
+        list:{
+          className:'',//<--set classname if you want
+          volume:'down',
+          axis:'y',
+          items:[{
+            label:<span>ITALIC</span>,
+            className:'',
+            mark:'italic'
+          },
+            {
+              label:<span>SAY_SOMETHING</span>,
+              className:'',
+            }
+          ]
+        }
+      },
+      {
+        label:<span>SUBMIT</span>,
+        className:''
+      },
+
+
+      {
+        label:<span>BOLD</span>,
+        className:'',
+        mark:'bold'
+      },
+
+      {
+        label:<span>CODE</span>,
+        className:'',
+        mark:'code'
+      }
+    ]
+}
+
+const schema = {
+  marks: {
+    bold: props => <strong>{props.children}</strong>,
+    code: props => <code>{props.children}</code>,
+    italic: props => <em>{props.children}</em>,
+    underlined: props => <u>{props.children}</u>,
+  }
+}
+
+export const CoreLayout = ({ children }) => (
+  <div className="container text-center">
+    <Header />
+
+    <EditorMenu provider={provider} schema={schema} />
+
+    {/*<HoveringMenu/>*/}
+
+    <div className="core-layout__viewport">
+      {children}
+    </div>
+  </div>
+)
+
+CoreLayout.propTypes = {
+  children: React.PropTypes.element.isRequired,
+}
+
+export default CoreLayout
